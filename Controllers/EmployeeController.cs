7b915fee84459ba849e593207580ae296e7c483a
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-12 10:00:18 +0200

    Employee Controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+    }
+}
