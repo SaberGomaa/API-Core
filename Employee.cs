e403e3960720f9b2cfd1a67f675619e38506fd29
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-12 09:59:09 +0200

    Employee Model

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace API_Core
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+        public string  Address { get; set; }
+
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
