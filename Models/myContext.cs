1d4f0e1e33e2aa59c984dfbdbbfd118346816993
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-12 10:35:59 +0200

    Register the database context

---
+++ Models/myContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Core.Models
+{
+    public class myContext : DbContext
+    {
+        public myContext(DbContextOptions<myContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> employees { get; set; } = null!;
+    }
+}
